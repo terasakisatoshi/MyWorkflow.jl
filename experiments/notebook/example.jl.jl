37e8d2fc7c378fd8a43813a18edf5801772426f1
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-01-30 02:26:24 +0900

    update

--- experiments/notebook/example.jl.jl
+++ experiments/notebook/example.jl.jl
@@ -5,25 +5,16 @@
 #       extension: .jl
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.3.2
+#       jupytext_version: 1.3.3
 #   kernelspec:
 #     display_name: Julia 1.3.1
 #     language: julia
 #     name: julia-1.3
 # ---
 
-println(ENV["PYTHON"])
-println(ENV["JUPYTER"])
-
-import Pkg
-Pkg.status()
+@show ENV["JUPYTER"]
+@show ENV["PYTHON"]
 
 using Example
-hello("WORLD")
 
-using Plots
-
-for t in 1:0.1:10
-    IJulia.clear_output(true)
-    plot(x-> sin(x+t)) |> display
-end
+hello("World")
