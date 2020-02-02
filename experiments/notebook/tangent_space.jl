ce41af12532674774722cc1229f3008900f9d61c
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-02-02 20:08:47 +0900

    update

--- experiments/notebook/tangent_space.jl
+++ experiments/notebook/tangent_space.jl
@@ -13,12 +13,14 @@
 #     name: julia-1.3
 # ---
 
+# +
+using Pkg
+Pkg.activate()
+
 using Plots
 using Distributions
 using LinearAlgebra
 using ForwardDiff
-
-gr()
 
 # +
 struct Point{T<:Real}
@@ -54,31 +56,28 @@ end
 zfunc(x, y) = -x^2-y^2+1.
 tp = Tp(zfunc, [3.0, 1.0])
 
-function visualize(tp::Tp; camera = (0, 30), opacity=0.5)
+function visualize(tp::Tp; camera = (0, 30))
     tp_offset=0.5
-    offset=3.0
+    offset=1.0
     tp_xs = range(tp.p.x - tp_offset, tp.p.x + tp_offset, length = 10)
     tp_ys = range(tp.p.y - tp_offset, tp.p.y + tp_offset, length = 10)
     xs = range(tp.p.x - offset, tp.p.x + offset, length = 20)
     ys = range(tp.p.y - offset, tp.p.y + offset, length = 20)
     p = plot(xlabel="x", ylabel="y")
-    wireframe!(p, xs, ys, (x, y) -> tp.zfunc(x, y), camera = camera, opacity=0.9)
-    surface!(p, tp_xs, tp_ys, (x, y) -> tangent_plane(tp, x, y), camera = camera, opacity=0.8)
+    wireframe!(p, xs, ys, (x, y) -> tp.zfunc(x, y), camera = camera)
+    surface!(p, tp_xs, tp_ys, (x, y) -> tangent_plane(tp, x, y), camera = camera)
     scatter3d!(p, [tp.p.x],[tp.p.y],[tp.p.z], label="p")
     return p
 end
 
-# +
 ps=[]
-
 for θ in 1:90
     IJulia.clear_output(true)
     p = visualize(tp, camera = (θ, 30))
     p |> display
     push!(ps, p)
 end
-# -
 
 @gif for p in ps
-    push!(ps, p)
+    plot(p)
 end
