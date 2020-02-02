e3825b1a7fb295414571e414fe9d1fcd56b0da6e
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-02-02 19:21:30 +0900

    fix tangent_space.jl

--- experiments/notebook/plots_sample.jl
+++ experiments/notebook/plots_sample.jl
@@ -16,7 +16,20 @@ print("Hello world")
 
 using Plots
 
+# # Rapid Visualization on Jupyter
+#
+# Just `add IJulia.clear_output(true)` and display `p` plot object simultaneously will improve your visualizatoin life
+
+ps=[]
 for t in 1:0.1:10
     IJulia.clear_output(true)
-    plot(x-> sin(x+t)) |> display
+    p = plot(x-> sin(x+t))
+    p |> display
+    push!(ps, p)
+end
+
+# # Dump result as tmp.gif
+
+@gif for p in ps
+    plot(p)
 end
