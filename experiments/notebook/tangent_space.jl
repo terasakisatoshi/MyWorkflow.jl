64bf630d5b79dbb233f489cd4203f9f45d952a29
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-02-02 19:21:40 +0900

    fix tangent_space.jl

--- experiments/notebook/tangent_space.jl
+++ experiments/notebook/tangent_space.jl
@@ -18,74 +18,67 @@ using Distributions
 using LinearAlgebra
 using ForwardDiff
 
+gr()
+
 # +
 struct Point{T<:Real}
     x::T
     y::T
+    z::T
 end
 
 
-Point(xy::Vector{T}) where {T} = Point{T}(T.(xy)...)
+Point(xyz::Vector{T}) where {T} = Point{T}(T.(xyz)...)
 
 # +
 struct Tp
     zfunc::Function
-    v::Vector{Float64}
-    w::Vector{Float64}
+    gx::Float64
+    gy::Float64
     p::Point{Float64}
-    invA::Matrix{Float64}
 end
 
 function Tp(zfunc, p_xy)
     zfuncwrap(xy) = zfunc(xy...)
     g = xy -> ForwardDiff.gradient(zfuncwrap, xy)
     ∇g_x, ∇g_y = g(p_xy)
-
-    v = [
-        1.0
-        0.0
-        ∇g_x
-    ]
-
-    w = [
-        0.0
-        1.0
-        ∇g_x
-    ]
-
-    v = normalize(v)
-    w = normalize(w)
-
-    A = [
-        v[1] w[1]
-        v[2] w[2]
-    ]
-
-    Tp(zfunc, v, w, Point(p_xy), inv(A))
+    Tp(zfunc, ∇g_x, ∇g_y, Point([p_xy..., zfuncwrap(p_xy)]))
 end
 
 function tangent_plane(tp::Tp, x, y)
-    z_vec = [tp.v[3] tp.w[3]] * tp.invA * [x, y]
-    z = z_vec[1] + tp.zfunc(tp.p.x, tp.p.y)
+    z = tp.gx * (x-tp.p.x) + tp.gy * (y-tp.p.y)+ tp.p.z
     return z
 end
 # -
 
-d = MvNormal([0.5, 0.5], I(2))
-zfunc(x, y) = pdf(d, [x, y])
-tp = Tp(zfunc, [0.0, 0.0])
+zfunc(x, y) = -x^2-y^2+1.
+tp = Tp(zfunc, [3.0, 1.0])
 
-function visualize(tp::Tp; camera = (0, 30))
-    tp_xs = range(tp.p.x - 0.5, tp.p.x + 0.5, length = 10)
-    tp_ys = range(tp.p.y - 0.5, tp.p.y + 0.5, length = 10)
-    xs = range(tp.p.x - 3.0, tp.p.x + 3.0, length = 20)
-    ys = range(tp.p.y - 3.0, tp.p.y + 3.0, length = 20)
-    p = plot()
-    wireframe!(p, tp_xs, tp_ys, (x, y) -> tangent_plane(tp, x, y), camera = camera)
-    surface!(p, xs, ys, (x, y) -> tp.zfunc(x, y), camera = camera)
+function visualize(tp::Tp; camera = (0, 30), opacity=0.5)
+    tp_offset=0.5
+    offset=3.0
+    tp_xs = range(tp.p.x - tp_offset, tp.p.x + tp_offset, length = 10)
+    tp_ys = range(tp.p.y - tp_offset, tp.p.y + tp_offset, length = 10)
+    xs = range(tp.p.x - offset, tp.p.x + offset, length = 20)
+    ys = range(tp.p.y - offset, tp.p.y + offset, length = 20)
+    p = plot(xlabel="x", ylabel="y")
+    wireframe!(p, xs, ys, (x, y) -> tp.zfunc(x, y), camera = camera, opacity=0.9)
+    surface!(p, tp_xs, tp_ys, (x, y) -> tangent_plane(tp, x, y), camera = camera, opacity=0.8)
+    scatter3d!(p, [tp.p.x],[tp.p.y],[tp.p.z], label="p")
     return p
 end
 
-@gif for θ in 1:180
-    visualize(tp, camera = (θ, 30))
+# +
+ps=[]
+
+for θ in 1:90
+    IJulia.clear_output(true)
+    p = visualize(tp, camera = (θ, 30))
+    p |> display
+    push!(ps, p)
+end
+# -
+
+@gif for p in ps
+    push!(ps, p)
 end
