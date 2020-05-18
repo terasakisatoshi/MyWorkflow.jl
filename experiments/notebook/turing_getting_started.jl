f08257bd128e15ba3376151688f312a1e3f4a784
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-05-18 16:51:27 +0900

    add coinflip example

--- experiments/notebook/turing_getting_started.jl
+++ experiments/notebook/turing_getting_started.jl
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -11,6 +12,10 @@
 #     language: julia
 #     name: julia-1.4
 # ---
+
+# # Turing.jl getting started
+#
+# - [Turing](https://turing.ml/dev/docs/using-turing/) is a general-purpose probabilistic programming language for robust, efficient Bayesian inference and decision making. 
 
 # +
 using Turing
@@ -31,8 +36,47 @@ chn = sample(gdemo(1.5, 2), HMC(0.1, 5), 1000)
 describe(chn)
 
 # Plot and save results
-p = plot(chn)
-savefig("gdemo-plot.png")
+plot(chn)
 # -
 
-p
+# # Probablistic Programming in Thirty Seconds
+#
+# - Taken from https://turing.ml/dev/docs/using-turing/quick-start
+
+# +
+# Import libraries.
+using Turing, StatsPlots, Random
+
+# Set the true probability of heads in a coin.
+p_true = 0.5
+
+# Iterate from having seen 0 observations to 100 observations.
+Ns = 0:100;
+
+# Draw data from a Bernoulli distribution, i.e. draw heads or tails.
+Random.seed!(12)
+data = rand(Bernoulli(p_true), last(Ns))
+
+# Declare our Turing model.
+@model coinflip(y) = begin
+    # Our prior belief about the probability of heads in a coin.
+    p ~ Beta(1, 1)
+
+    # The number of observations.
+    N = length(y)
+    for n in 1:N
+        # Heads or tails of a coin are drawn from a Bernoulli distribution.
+        y[n] ~ Bernoulli(p)
+    end
+end;
+
+# Settings of the Hamiltonian Monte Carlo (HMC) sampler.
+iterations = 1000
+ϵ = 0.05
+τ = 10
+
+# Start sampling.
+chain = sample(coinflip(data), HMC(ϵ, τ), iterations);
+
+# Plot a summary of the sampling process for the parameter p, i.e. the probability of heads in a coin.
+histogram(chain[:p])
