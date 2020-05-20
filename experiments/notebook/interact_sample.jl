21598c210dafaf80848cb66694bd6a7694a3c45b
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-05-20 13:08:06 +0900

    update display name

--- experiments/notebook/interact_sample.jl
+++ experiments/notebook/interact_sample.jl
@@ -7,7 +7,7 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.4.2
 #   kernelspec:
-#     display_name: Julia 1.4.0
+#     display_name: Julia 1.4.1
 #     language: julia
 #     name: julia-1.4
 # ---
